6ba1a9f8414081474a67c360d315fe9bff964582
Davekoder <davekoder@gmail.com>
2020-06-23 20:46:14 +0800

    Updates

---
+++ precache-manifest.48d36999a2309460604dcb3850bcc1cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05c1a6efae0e20d1827f9099df48ab5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7d8f65262acffd1aec42",
+    "url": "/monster-rolodex/static/css/main.466b2878.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8f65262acffd1aec42",
+    "url": "/monster-rolodex/static/js/main.eca41939.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
